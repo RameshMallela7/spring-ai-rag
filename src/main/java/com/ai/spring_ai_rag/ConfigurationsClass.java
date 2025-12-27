299f64535cedb2aaaf7a7d660a7c4cff7da3b7f0
Mallela, Ramesh Babu <ramesh.babu.mallela@merck.com>
2025-12-27 14:18:30 +0530

    reolved bean issue

--- src/main/java/com/ai/spring_ai_rag/ConfigurationsClass.java
+++ src/main/java/com/ai/spring_ai_rag/ConfigurationsClass.java
@@ -1,14 +1,38 @@
 package com.ai.spring_ai_rag;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.regions.providers.AwsRegionProvider;
 
 
 @Configuration
 public class ConfigurationsClass {
 
-  
+    @Value("${spring.ai.bedrock.aws.region:ap-south-1}")
+    private String awsRegion;
+
+    @Value("${spring.ai.bedrock.aws.access-key}")
+    private String accessKey;
+
+    @Value("${spring.ai.bedrock.aws.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public AwsRegionProvider regionProvider() {
+        return () -> Region.of(awsRegion);
+    }
+
+    @Bean
+    public AwsCredentialsProvider credentialsProvider() {
+        return StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(accessKey, secretKey)
+        );
+    }
 
 }
--- src/main/java/com/ai/spring_ai_rag/service/NormalPromptService.java
+++ src/main/java/com/ai/spring_ai_rag/service/NormalPromptService.java
@@ -3,17 +3,24 @@ package com.ai.spring_ai_rag.service;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeAsyncClient;
+
 @Service
 public class NormalPromptService {
 
-    private ChatClient chatClient;
+    private final ChatClient chatClient;
 
-   public NormalPromptService(ChatClient.Builder chatClientBuilder) {
-       this.chatClient = chatClientBuilder.build();
-   }
+    public NormalPromptService(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+    
 
     public String normalProcessChat(String userMessage) {
-        String response = chatClient.prompt(userMessage).call().content();
+        String response = chatClient.prompt().user(userMessage).call().content();
         return "Processed message: " + response;
     }
 }
