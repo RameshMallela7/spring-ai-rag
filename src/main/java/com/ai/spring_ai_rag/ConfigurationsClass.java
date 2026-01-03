c1c3b72a427ace032061c525dd30a2ad6431143b
Ramesh <mallelaramesh5@gmail>
2026-01-03 14:49:13 +0530

    updated RAG code

--- src/main/java/com/ai/spring_ai_rag/ConfigurationsClass.java
+++ src/main/java/com/ai/spring_ai_rag/ConfigurationsClass.java
@@ -1,10 +1,49 @@
 package com.ai.spring_ai_rag;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
+
+import java.util.logging.Logger;
+
+import org.springframework.ai.bedrock.cohere.BedrockCohereEmbeddingModel;
+import org.springframework.ai.bedrock.cohere.api.CohereEmbeddingBedrockApi;
+import org.springframework.ai.bedrock.titan.BedrockTitanEmbeddingModel;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 
 @Configuration
 public class ConfigurationsClass {
-  
+
+    private static final Logger logger = Logger.getLogger(ConfigurationsClass.class.getName());
+
+
+   /* 
+    @Bean
+    @Primary
+    public EmbeddingModel embeddingModel(BedrockRuntimeClient client) {
+        return new BedrockCohereEmbeddingModel(new CohereEmbeddingBedrockApi("cohere.embed-english-v3", "us-east-1")
+        		
+        		
+               // client,
+                //"amazon.nova-2-multimodal-embeddings-v1:0"
+        );
+    }*/
+    @Bean
+    @Primary
+    public EmbeddingModel embeddedMode(
+        @Qualifier("titanEmbeddingModel") EmbeddingModel titanEmbeddingModel
+    ) {
+        logger.info("ConfigurationsClass : embeddedMode @bean - Using Titan Embedding Model as Primary Embedding Model.");
+        return titanEmbeddingModel;
+    }
+
+
+    
+
+    
+    
 }
--- src/main/java/com/ai/spring_ai_rag/service/DocumentInjectionService.java
+++ src/main/java/com/ai/spring_ai_rag/service/DocumentInjectionService.java
@@ -1,12 +1,9 @@
 
 package com.ai.spring_ai_rag.service;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
-
+import java.util.Objects;
+import java.util.logging.Logger;
 
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
@@ -15,40 +12,91 @@ import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.simple.JdbcClient;
-
+import org.springframework.stereotype.Service;
 
 //  --1--
 
 @Service
-public class DocumentInjectionService implements CommandLineRunner 
-{
-    
+public class DocumentInjectionService implements CommandLineRunner {
 
-    @Autowired
-    private JdbcClient jdbcClient;
+	Logger logger = Logger.getLogger(DocumentInjectionService.class.getName());
 
-    @Autowired
-    private VectorStore vectorStore;
+	@Autowired
+	private JdbcClient jdbcClient;
 
-    @Value("classpath:/pdf/Unique_Dataset.pdf")
-    private Resource path;
+	@Autowired
+	private VectorStore vectorStore;
 
+	@Value("classpath:/pdf/Unique_Dataset-1.pdf")
+	private Resource path;
 
-    @Override
-    public void run(String... args) throws Exception {
-        // Initialization logic here
-        Integer count = jdbcClient.sql("SELECT count(*) FROM vector_store").query(Integer.class).single();
-        if (count == 0) {
-            TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(path);
-            //List<Document> documents = tikaDocumentReader.readDocumentsFromDirectory("path/to/your/documents");
+	@Override
+	public void run(String... args) throws Exception {
+		// Initialization logic here
+		logger.info("DocumentInjectionService : Checking and injecting documents into Vector Store if needed...");
+		Integer count = jdbcClient.sql("SELECT count(*) FROM vector_store").query(Integer.class).single();
+		if (count == 0) {
+			logger.info("DocumentInjectionService : No documents found in Vector Store. Injecting documents...");
+			TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(path);
+			// List<Document> documents =
+			// tikaDocumentReader.readDocumentsFromDirectory("path/to/your/documents");
 
-            TextSplitter textSplitter = new TokenTextSplitter();
-            List<Document> textChunks = textSplitter.split(tikaDocumentReader.read());
+			List<Document> rawDocs = tikaDocumentReader.read();
+			List<Document> cleanTextDocs = rawDocs.stream().map(doc -> sanitizeDocument(doc)).filter(Objects::nonNull)
+					.toList();
 
-            vectorStore.accept(textChunks);
-        }
-    }
-    
-     
+			TextSplitter textSplitter = new TokenTextSplitter();
+			List<Document> textChunks = textSplitter.split(cleanTextDocs);
+			logger.info("DocumentInjectionService : Number of document chunks to be injected: " + textChunks.size());
+
+			List<Document> safeChunks = textChunks.stream().filter(this::isPureText).toList();
+			logger.info("DocumentInjectionService : Is base64: " + isValidBase64(safeChunks));
+			vectorStore.add(safeChunks);
+			logger.info("DocumentInjectionService : Document injection completed.");
+		}
+
+	}
+
+	private Document sanitizeDocument(Document doc) {
+		if (doc == null || doc.getText() == null) {
+			return null;
+		}
+
+		String text = doc.getText();
+
+		// Remove non-printable and binary characters
+		text = text.replaceAll("[^\\p{Print}\\p{Space}]", "");
+
+		// Skip garbage / image blobs
+		if (text.length() < 50) {
+			return null;
+		}
+
+		return new Document(text, doc.getMetadata());
+	}
+
+	private boolean isPureText(Document doc) {
+		return doc.getText().chars().allMatch(c -> c == '\n' || c == '\r' || c == '\t' || (c >= 32 && c <= 126));
+	}
+
+	private boolean isValidBase64(List<Document> documents) {
+		try {
+			logger.info("DocumentInjectionService-isValidBase64 : Validating base64 string..."
+					+ documents.get(0).getText());
+			logger.info("DocumentInjectionService-isValidBase64 : Validating base64 getFormattedContent..."
+					+ documents.get(0).getFormattedContent());
+			logger.info("DocumentInjectionService-isValidBase64 : Validating base64 media..."
+					+ documents.get(0).getMedia());
+			logger.info("DocumentInjectionService-isValidBase64 : Validating base64 metadata..."
+					+ documents.get(0).getMetadata());
+			java.util.Base64.getDecoder().decode(documents.get(0).getText());
+			return true;
+		} catch (IllegalArgumentException e) {
+			return false;
+		}
+	}
+
 }
